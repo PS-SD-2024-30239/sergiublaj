00ab1d2a36225b505fc9d803fdfbab309c198278
sergiublaj <s3rgiumr@gmail.com>
2024-04-10 23:28:31 +0300

    refactored request interceptor

--- chef-mgmt-frontend/src/app/core/http-interceptor/request-interceptor.ts
+++ chef-mgmt-frontend/src/app/core/http-interceptor/request-interceptor.ts
@@ -1,20 +1,23 @@
-import {HttpHeaders, HttpInterceptorFn} from "@angular/common/http";
-import {inject} from "@angular/core";
-import {CookieService} from "ngx-cookie-service";
+import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 
 export const requestInterceptor: HttpInterceptorFn = (req, next) => {
-  const cookieService = inject(CookieService);
-  const jwtToken = cookieService.get('jwt-token');
-  const headers = new HttpHeaders({
-    'Authorization': 'Bearer ' + jwtToken
-  });
-
   const modifiedReq = req.clone({
     url: 'http://localhost:8777/api/' + req.url,
-    withCredentials: true,
-    headers
+    headers: getHeaders(req.url, req.headers),
+    withCredentials: true
   });
 
   return next(modifiedReq);
+};
+
+const getHeaders = (url: string, httpHeaders: HttpHeaders): HttpHeaders => {
+  const cookieService = inject(CookieService);
+  const jwtToken = cookieService.get('jwt-token');
+
+  return url.includes('auth')
+    ? new HttpHeaders()
+    : new HttpHeaders({ 'Authorization': `Bearer ${jwtToken}` });
 };
